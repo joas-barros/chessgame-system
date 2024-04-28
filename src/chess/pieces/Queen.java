8f7976ca88911b7a8ac39d67e9465db7c5b6080e
joas-barros <joas.barros14@hotmail.com>
2024-04-28 17:29:05 -0300

    Bishop

---
+++ src/chess/pieces/Queen.java
@@ -0,0 +1,21 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Queen extends ChessPiece {
+    public Queen(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        return new boolean[0][];
+    }
+
+    @Override
+    public String toString() {
+        return "Q";
+    }
+}
